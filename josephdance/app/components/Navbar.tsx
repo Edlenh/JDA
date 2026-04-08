31d11acdfa46a429a0bca05c64f1876142e98135
Edward <herreraed93@gmail.com>
2026-04-08 16:23:16 -0700

    bg color

--- josephdance/app/components/Navbar.tsx
+++ josephdance/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-4 bg-white border-b">
+    <nav className="flex justify-between items-center p-4">
     
       <div className="flex gap-4">
       
